a68b51ca931913caff69c9eba09c5a578db03074
Naveengali <naveengali80@gmail.com>
2021-05-26 19:07:34 +0530

    Created Page Components and also enabled navigation between pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Resume from './pages/Resume';
+import Starred from './pages/Starred';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route exact path="/Resume">
+          <Resume />
+        </Route>
+
+        <Route>This is 404 Page Go Away from here</Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
