4957e1ecdced76d0776a7266a17000d1e53f58d4
Naveengali <naveengali80@gmail.com>
2021-05-31 11:27:47 +0530

    Tested Loding SCreens and Error Message

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,20 +6,36 @@ const Show = () => {
   const { id } = useParams();
 
   const [show, setshow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setshow(results);
-    });
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        setTimeout(() => {
+          setshow(results);
+          setIsLoading(false);
+        }, 2000);
+      })
+      .catch(err => {
+        setError(err.message);
+        setIsLoading(false);
+      });
   }, [id]);
 
-  // eslint-disable-next-line no-console
   console.log('show', show);
 
+  if (isLoading) {
+    return <div>Data is Loading</div>;
+  }
+
+  if (error) {
+    return <div>Error Occured: {error}</div>;
+  }
   return (
-    <>
+    <div>
       <h2>This is Show Page</h2>
-    </>
+    </div>
   );
 };
 export default Show;
