4f3bb98e7bac3caf50059635320b2efec97d5145
Naveengali <naveengali80@gmail.com>
2021-05-31 14:55:53 +0530

    Completed adding loading screen and error message

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,24 +5,33 @@ import { apiGet } from '../misc/config';
 const Show = () => {
   const { id } = useParams();
 
-  const [show, setshow] = useState(null);
+  const [show, setShow] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    let isMounted = true;
+
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(results => {
-        setTimeout(() => {
-          setshow(results);
+        if (isMounted) {
+          setShow(results);
           setIsLoading(false);
-        }, 2000);
+        }
       })
       .catch(err => {
-        setError(err.message);
-        setIsLoading(false);
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
       });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
+  // eslint-disable-next-line no-console
   console.log('show', show);
 
   if (isLoading) {
