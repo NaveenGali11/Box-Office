cd121f99410488bd1cd20c73a670c6c1717ebab8
Naveengali <naveengali80@gmail.com>
2021-06-06 07:25:57 +0530

    Edited ServiceWorker and rehosted with gh-pages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+// import * as serviceWorker from './serviceWorker';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
